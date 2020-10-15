ca74d64952e4e3ecfa1fd67c67518027a0a421ba
Serg <ochkarik1983@mail.ru@>
2020-10-15 19:32:57 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -364,8 +364,8 @@ function home_bindEvents() {
     }),
 
     on('.bottom-bar', 'click', '.sound-container', function() {switchVolume()})
-    //on('body', 'click', '.special-link', function() {console.log("changeCapsule"), changeCapsule(this)})
-    $(".special-link").on("click", changeCapsule),
+    on('body', 'click', '.special-link', function() {_this=this, changeCapsule(_this)})
+    //$(".special-link").on("click", changeCapsule),
     $(".special-popup").on("click", ".menu-container .menu-item", changeCapsule),
     $(".dinamic-popup").on("click", ".popup-content .close-button", closeGeneralPopup),
     $(".special-popup").on("click", ".popup-content .close-button", closeSpecialPopup),
